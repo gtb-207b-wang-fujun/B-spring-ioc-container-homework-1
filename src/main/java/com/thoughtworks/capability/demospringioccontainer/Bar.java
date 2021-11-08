7768a63eb2a75389b3daf5a302d8366522c2dc6f
Fujun.Wang <1064980344@qq.com>
2021-11-08 22:19:12 +0800

    [Wang Fujun] Feature :Third Answer using PostConstruct

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/Bar.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Bar.java
@@ -1,15 +1,17 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
-import org.springframework.context.annotation.Lazy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 
 @Component
 public class Bar {
-
+    @Autowired
     private Foo foo;
-    @Lazy
-    public Bar(Foo foo) {
-        this.foo = foo;
+    @PostConstruct
+    public void init() {
+        foo.setBar(this);
     }
 
     public void hi() {
